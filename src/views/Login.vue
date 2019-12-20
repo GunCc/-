c7d3271c7e1a6f35eb590d6b24fa6f850c1d7dd0
zhangzikang <2859893460@qq.com>
2019-12-20 14:51:17 +0800

    第一次提交

---
+++ src/views/Login.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="bg">
+    <div class="box">
+      <span class="title">登录</span>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.bg {
+  background-image: url("../assets/bg.png");
+  height: 100vh;
+  width: 100%;
+  background-size: cover;
+  overflow: hidden;
+  .box {
+    background: #fff;
+    box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.3);
+    width: 400px;
+    margin: 200px auto;
+    padding: 12px 15px;
+    border-radius: 5px;
+  }
+}
+</style>
